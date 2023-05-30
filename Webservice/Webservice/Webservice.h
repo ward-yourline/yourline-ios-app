24bfffcfd49b078d08c1e87b1777afe02ce240d0
YourLine <103743058+ward-yourline@users.noreply.github.com>
2023-05-30 13:37:39 +0100

    Domain and webservice frameworks

---
+++ Webservice/Webservice/Webservice.h
@@ -0,0 +1,18 @@
+//
+//  Webservice.h
+//  Webservice
+//
+//  Created by Warrd Adlani on 30/05/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Webservice.
+FOUNDATION_EXPORT double WebserviceVersionNumber;
+
+//! Project version string for Webservice.
+FOUNDATION_EXPORT const unsigned char WebserviceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Webservice/PublicHeader.h>
+
+
